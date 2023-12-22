f6d4a4d9e576b1808dad77591b6523492f32b841
David Márquez Mínguez <davidmarquezminguez@gmail.com>
2023-12-22 13:34:39 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# ToDo list using Flask
+
+## How To Run
+1. Install `virtualenv`:
+```
+$ pip install virtualenv
+```
+
+2. Open a terminal in the project root directory and run:
+```
+$ virtualenv env
+```
+
+3. Then run the command:
+```
+$ .\env\Scripts\activate
+```
+
+4. Finally start the web server:
+```
+$ (env) python app.py
+```
---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(100), nullable=False)
+  created = db.Column(db.DateTime, default=datetime.utcnow)
+
+  def __repr__(self):
+    ''' Return a string as a representation of object'''
+    return '<Task %r>' % self.id
+
+
+
+
+
+
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+  if request.method == 'POST':
+    task_content = request.form['title']
+    new_task = Todo(title=task_content)
+    try:
+      db.session.add(new_task)
+      db.session.commit()
+      return redirect('/')
+    except:
+      return 'There was an issue adding your task'
+  else:
+    tasks = Todo.query.order_by(Todo.created).all()
+    return render_template('index.html', tasks=tasks)
+
+if __name__ == "__main__":
+  app.run(debug=True)
