9c0a7e5e6773041043bfa99acda25d91f1ba2013
David Márquez Mínguez <davidmarquezminguez@gmail.com>
2023-12-24 18:57:53 +0100

    Update action

--- app.py
+++ app.py
@@ -10,24 +10,18 @@ class Todo(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   title = db.Column(db.String(100), nullable=False)
   created = db.Column(db.DateTime, default=datetime.utcnow)
+  completed = db.Column(db.Boolean, default=False)
 
   def __repr__(self):
     ''' Return a string as a representation of object'''
     return '<Task %r>' % self.id
 
-
-
-
-
-
-
-
 @app.route('/', methods=['POST', 'GET'])
 def index():
   ''' Creates new tasks '''
   if request.method == 'POST':
-    task_content = request.form['title']
-    new_task = Todo(title=task_content)
+    task_title = request.form['title']
+    new_task = Todo(title=task_title)
     try:
       db.session.add(new_task)
       db.session.commit()
@@ -48,6 +42,20 @@ def delete(id):
     return redirect('/')
   except:
     return 'There was an issue deleting your task'
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+  ''' Update a single task'''
+  updated_task = Todo.query.get_or_404(id)
+  if request.method == 'POST':
+    updated_task.title = request.form['task_title']
+    try:
+      db.session.commit()
+      return redirect('/')
+    except:
+      return 'There was an issue updating your task'
+  return redirect('/')
+
 
 if __name__ == "__main__":
   app.run(debug=True)
