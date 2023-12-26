9b0c246cff4331ef14f7a78a0c5ab602cf800367
David Márquez Mínguez <davidmarquezminguez@gmail.com>
2023-12-26 12:22:09 +0100

    Serveless function

--- src/app.py
+++ src/app.py
@@ -56,7 +56,7 @@ def update(id):
   return redirect('/')
 
 
-'''if __name__ == "__main__":
+if __name__ == "__main__":
   app.app_context().push()
   db.create_all()
-  app.run(debug=True)'''
+  app.run(debug=True)
