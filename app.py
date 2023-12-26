bba91bf69f5a6555d3f940f77f6af4363d4d5f5d
David Márquez Mínguez <davidmarquezminguez@gmail.com>
2023-12-26 11:14:03 +0100

    Design modifications

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ class Todo(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   title = db.Column(db.String(100), nullable=False)
   created = db.Column(db.DateTime, default=datetime.utcnow)
-  completed = db.Column(db.Boolean, default=False)
 
   def __repr__(self):
     ''' Return a string as a representation of object'''
